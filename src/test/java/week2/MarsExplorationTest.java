2ac6ed053ad3fc4fe1941131cd87debdb6083224
ttg <ttg@elca.vn>
2025-01-23 10:03:15 +0700

    MarsExploration

---
+++ src/test/java/week2/MarsExplorationTest.java
@@ -0,0 +1,43 @@
+package week2;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+import ttg.traning.week2.marsExploration.MarsExploration;
+import ttg.traning.week2.pangrams.Pangrams;
+
+/**
+ *
+ * @author ttg
+ */
+@RunWith(DataProviderRunner.class)
+public class MarsExplorationTest {
+
+    @DataProvider
+    public static Object[][] provideDataForTestMarsExploration() {
+
+        String input1 = "SOSSOT";
+        int output1 = 1;
+
+        String input2 = "SOSSPSSQSSOR";
+        int output2 = 3;
+
+        return new Object[][] {
+//                {input1, output1},
+                {input2, output2}
+        };
+    }
+
+
+    @Test
+    @UseDataProvider("provideDataForTestMarsExploration")
+    public void testMarsExploration(String input, int expectedOutput) {
+        int actualOutput = MarsExploration.process1(input);
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+}
