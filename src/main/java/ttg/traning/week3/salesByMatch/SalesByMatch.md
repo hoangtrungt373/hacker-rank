b7a75af7aaaea3b34af5f7a63594015b4e87ceef
ttg <ttg@elca.vn>
2025-02-04 11:17:47 +0700

    SalesByMatch

---
+++ src/main/java/ttg/traning/week3/salesByMatch/SalesByMatch.md
@@ -0,0 +1,26 @@
+
+# SalesByMatch
+
+There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock,
+determine how many pairs of socks with matching colors there are.
+
+
+
+## Example
+
+    Ex 1
+    ar = [1, 2, 1, 2, 1, 3, 2]
+    one pair of color 1, one pair of color 2 -> The number of pairs is 2
+
+    Ex 2
+    ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    two pair of color 10, one pair of color 20 -> The number of pairs is 3
+
+## Function Description
+
+**sockMerchant** has the following parameter(s):
+
+* int ar[n]: the colors of each sock
+
+## Returns
+* int: the number of pairs
---
+++ src/test/java/week3/SalesByMatchTest.java
@@ -0,0 +1,46 @@
+package week3;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+import ttg.traning.week3.salesByMatch.SalesByMatch;
+import ttg.traning.week3.subarrayDivision2.SubarrayDivision2;
+
+/**
+ *
+ * @author ttg
+ */
+@RunWith(DataProviderRunner.class)
+public class SalesByMatchTest {
+
+    @DataProvider
+    public static Object[][] provideDataForTestSalesByMatch() {
+
+        List<Integer> list_1 = Arrays.asList(1, 2, 1, 2, 1, 3, 2);
+        int output_1 = 2;
+
+        List<Integer> list_2 = Arrays.asList(10, 20, 20, 10, 10, 30, 50, 10, 20);
+        int output_2 = 3;
+
+        return new Object[][] {
+                {list_1, output_1},
+                {list_2, output_2},
+        };
+    }
+
+
+    @Test
+    @UseDataProvider("provideDataForTestSalesByMatch")
+    public void testSalesByMatch(List<Integer> list, Integer expectedOutput) {
+        Integer actualOutput = SalesByMatch.process1(list);
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+}
